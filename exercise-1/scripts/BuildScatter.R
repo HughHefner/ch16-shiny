a8ec965bb85a5ff78bc97936b2a73c5368ddb54a
unknown <chesdude@gmail.com>
2017-11-14 13:54:39 -0800

    yay

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,12 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+library("ggplot2")
+data <- 1:500
+
+newFunction <- function(data, x, y, color, title = "Title", xtitle = "X Title", ytitle = "Y Title") {
+  usa.map <- ggplot(data) +
+    labs(title = title, 
+         x = xtitle, 
+         y = ytitle)
+}
